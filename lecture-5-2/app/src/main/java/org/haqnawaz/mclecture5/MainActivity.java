a7d3890906623944338aa66fa7fe90d9f07cc8e7
BSEF8M014 <80028115+BSEF8M014@users.noreply.github.com>
2021-03-10 17:03:13 +0500

    lecture-5-2

---
+++ lecture-5-2/app/src/main/java/org/haqnawaz/mclecture5/MainActivity.java
@@ -0,0 +1,26 @@
+package org.haqnawaz.mclecture5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.button4);
+        textView = findViewById(R.id.textViewCode);
+    }
+
+    public void SomeOneIsThere(View view) {
+        textView.setText("Someone is there.");
+    }
+}
+
+
